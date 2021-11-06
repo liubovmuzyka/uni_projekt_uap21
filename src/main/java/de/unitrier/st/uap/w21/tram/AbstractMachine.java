afc5dbe42ab0b6fd500c558ae93d04ae056ffe23
liubovmuzyka <github@m.colba.ch>
2021-11-06 15:19:12 +0100

    tram

---
+++ src/main/java/de/unitrier/st/uap/w21/tram/AbstractMachine.java
@@ -0,0 +1,4 @@
+package de.unitrier.st.uap.w21.tram;
+
+public class AbstractMachine {
+}
---
+++ src/main/java/de/unitrier/st/uap/w21/tram/Main.java
@@ -0,0 +1,12 @@
+
+package de.unitrier.st.uap.w21.tram;
+final class Main
+{
+	private Main(){}
+	
+	public static void main(String[] argv)
+	{
+		// TODO: Create an instance of the abstract machine with respective parameters
+
+	}
+}
