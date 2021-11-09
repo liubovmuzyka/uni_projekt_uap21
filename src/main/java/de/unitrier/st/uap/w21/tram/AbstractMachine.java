a46501568445e51269c24a2dee49e89da7daa909
liubovmuzyka <github@m.colba.ch>
2021-11-09 02:41:15 +0100

    withoutLog4j

--- src/main/java/de/unitrier/st/uap/w21/tram/AbstractMachine.java
+++ src/main/java/de/unitrier/st/uap/w21/tram/AbstractMachine.java
@@ -1,223 +1,321 @@
 package de.unitrier.st.uap.w21.tram;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import java.util.ArrayList;
+
 
 public class AbstractMachine {
 
+    static Logger logger = Logger.getLogger(AbstractMachine.class);
+
+    public boolean verbose = true;
+
     final Instruction[] i;
-    int pc = 0, pp = -1, fp = -1, top = -1;
+    int pc = 0, pp = 0, fp = 0, top = -1;
     ArrayList<Integer> stack = new ArrayList<>();
+
 
     public AbstractMachine(Instruction[] i) {
         this.i = i;
+        PropertyConfigurator.configure("src/log4j.properties");
+
+        // adoption for examples 1 and 2
+        if (top == 1) {
+            stack.add(0);
+            stack.add(0);
+        }
+        if (top == 0) {
+            stack.add(0);
+        }
     }
 
     public String interpret() {
 
-        while(pc >= 0) {
+        while (pc >= 0) {
             switch (i[pc].getOpcode()) {
                 case Instruction.CONST:
                     conzt(i[pc].getArg1());
+                    break;
+                case Instruction.LOAD:
+                    if (i[pc].getArg2() == null) {
+                        load(i[pc].getArg1());
+                        break;
+                    } else {
+                        expandedLoad(i[pc].getArg1(), i[pc].getArg2());
+                        break;
+                    }
+                case Instruction.STORE:
+                    if (i[pc].getArg2() == null) {
+                        store(i[pc].getArg1());
+                        break;
+                    } else {
+                        expandedStore(i[pc].getArg1(), i[pc].getArg2());
+                        break;
+                    }
+                case Instruction.ADD:
+                    add();
+                    break;
+                case Instruction.SUB:
+                    sub();
+                    break;
+                case Instruction.MUL:
+                    mul();
+                    break;
+                case Instruction.DIV:
+                    div();
+                    break;
+                case Instruction.LT:
+                    lt();
+                    break;
+                case Instruction.GT:
+                    gt();
+                    break;
+                case Instruction.EQ:
+                    eq();
+                    break;
+                case Instruction.NEQ:
+                    neq();
+                    break;
+                case Instruction.IFZERO:
+                    ifzero(i[pc].getArg1());
+                    break;
+                case Instruction.GOTO:
+                    gotou(i[pc].getArg1());
+                    break;
+                case Instruction.HALT:
+                    halt();
+                    break;
+                case Instruction.NOP:
+                    nop();
+                    break;
+                case Instruction.INVOKE:
+                    invoke(i[pc].getArg1(), i[pc].getArg2(), i[pc].getArg3());
+                    break;
+                case Instruction.RETURN:
+                    returnn();
+                    break;
+                case Instruction.POP:
+                    pop();
+                    break;
             }
         }
-
-
-
-
-        return "GGG";
+        return "Result: " + stack.get(top);
     }
 
     private void conzt(int p1) {
-        //if(stack.size() < top)
-        //    stack.add(p1);
-        //else
-        stack.add(0);
-        stack.set(top+1, p1);
+        stack.add(top + 1, p1);
         top++;
         pc++;
+        printout("conzt");
     }
+
     private void load(int p1) {
-        //if(stack.size() < top)
-        //    stack.add(stack.get(pp+p1));
-        //else
-        stack.add(0);
-        stack.set(top+1, stack.get(pp+p1));
+        if (stack.size() < top) {
+            int toptemp = top;
+            while (toptemp >= 0) {
+                stack.add(0);
+                toptemp--;
+            }
+        }
+        while (stack.size() <= pp + p1 || stack.size() <= top + 1)
+            stack.add(0);
+        stack.set(top + 1, stack.get(pp + p1));
         top++;
         pc++;
+        printout("load");
     }
+
+    private void expandedLoad(int p1, int p2) {
+        if (stack.size() < top) {
+            int toptemp = top;
+            while (toptemp >= 0) {
+                stack.add(0);
+                toptemp--;
+            }
+        }
+        while (stack.size() <= pp + p1 || stack.size() <= top + 1)
+            stack.add(0);
+        stack.set(top + 1, stack.get(spp(p2, pp, fp) + p1));
+        top++;
+        pc++;
+        printout("load");
+    }
+
     private void store(int p1) {
-        stack.set(pp+p1, stack.get(top));
+        while (stack.size() <= pp + p1 || stack.size() <= top)
+            stack.add(0);
+        stack.set(pp + p1, stack.get(top));
         top--;
         pc++;
+        printout("store");
     }
+
+    private void expandedStore(int p1, int p2) {
+        while (stack.size() <= pp + p1 || stack.size() <= top)
+            stack.add(0);
+        stack.set(spp(p2, pp, fp) + p1, stack.get(top));
+        top--;
+        pc++;
+        printout("store");
+    }
+
     private void add() {
-        stack.set(top-1, stack.get(top-1)+stack.get(top));
+        stack.set(top - 1, stack.get(top - 1) + stack.get(top));
         top--;
         pc++;
+        printout("add");
     }
+
     private void sub() {
-        stack.set(top-1, stack.get(top-1)-stack.get(top));
+        stack.set(top - 1, stack.get(top - 1) - stack.get(top));
         top--;
         pc++;
+        printout("sub");
     }
+
     private void mul() {
-        stack.set(top-1, stack.get(top-1)*stack.get(top));
+        stack.set(top - 1, stack.get(top - 1) * stack.get(top));
         top--;
         pc++;
+        printout("mul");
     }
+
+    public void printout(String op) {
+        if (verbose) {
+            String[] stackStrings = new String[stack.size()];
+            for (int i = 0; i < stackStrings.length; i++) {
+                if (i <= top) {
+                    stackStrings[i] = stack.get(i).toString();
+                } else {
+                    stackStrings[i] = "(" + stack.get(i).toString() + ")";
+                }
+            }
+            logger.info("top=" + top + ", " + "pc=" + pc + ", " + "pp=" + pp + ", " + "fp=" + fp + ", [" + String.join(", ", stackStrings) + "]" + (op == null ? "" : (" // " + op)));
+            // LOGGER.info("top=" + top + ", " + "pc=" + pc + ", " + "pp=" + pp + ", " + "fp=" + fp + ", [" + String.join(", ", stackStrings) + "]" + (op == null ? "" : (" // " + op)));
+        }
+    }
+
     private void div() {
-        stack.set(top-1, stack.get(top-1)/stack.get(top));
+        stack.set(top - 1, stack.get(top - 1) / stack.get(top));
         top--;
         pc++;
+        printout("div");
     }
+
     private void lt() {
-        if(stack.get(top-1) < stack.get(top)) {
-           stack.set(top-1, 1);
-        } else{
-            stack.set(top-1, 0);
+        if (stack.get(top - 1) < stack.get(top)) {
+            stack.set(top - 1, 1);
+        } else {
+            stack.set(top - 1, 0);
         }
         top--;
         pc++;
+        printout("lt");
     }
 
     private void gt() {
-        if(stack.get(top-1) > stack.get(top)) {
-            stack.set(top-1, 1);
-        } else{
-            stack.set(top-1, 0);
+        if (stack.get(top - 1) > stack.get(top)) {
+            stack.set(top - 1, 1);
+        } else {
+            stack.set(top - 1, 0);
         }
         top--;
         pc++;
+        printout("gt");
     }
 
     private void eq() {
-        if(stack.get(top-1) == stack.get(top)) {
-            stack.set(top-1, 1);
-        } else{
-            stack.set(top-1, 0);
+        if (stack.get(top - 1) == stack.get(top)) {
+            stack.set(top - 1, 1);
+        } else {
+            stack.set(top - 1, 0);
         }
         top--;
         pc++;
+        printout("eq");
     }
 
     private void neq() {
-        if(stack.get(top-1) != stack.get(top)) {
-            stack.set(top-1, 1);
-        } else{
-            stack.set(top-1, 0);
+        if (stack.get(top - 1) != stack.get(top)) {
+            stack.set(top - 1, 1);
+        } else {
+            stack.set(top - 1, 0);
         }
         top--;
         pc++;
+        printout("neq");
     }
 
     private void ifzero(int p1) {
-        if (stack.get(top)==0){
+        if (stack.get(top) == 0) {
             pc = p1;
-        } else{
+        } else {
             pc++;
         }
         top--;
+        printout("ifzero");
     }
 
     private void gotou(int p1) {
         pc = p1;
+        printout("gotou");
     }
 
     private void halt() {
         pc = -1;
+        printout("halt");
     }
 
     private void nop() {
         pc++;
+        printout("nop");
     }
 
     private void invoke(int p1, int p2, int p3) {
-        stack.add(0);
-        stack.add(0);
-        stack.add(0);
-        stack.add(0);
-        stack.add(0);
-        stack.set(top+1, pc+1);
-        stack.set(top+2, pp);
-        stack.set(top+3, fp);
-        stack.set(top+4, spp(p3, pp, fp));
-        stack.set(top+5, sfp(p3, pp, fp));
-        pp = top-p1+1;
+        while (stack.size() <= top + 5)
+            stack.add(0);
+        stack.set(top + 1, pc + 1);
+        stack.set(top + 2, pp);
+        stack.set(top + 3, fp);
+        stack.set(top + 4, spp(p3, pp, fp));
+        stack.set(top + 5, sfp(p3, pp, fp));
+        pp = top - p1 + 1;
         fp = top + 1;
         top = top + 5;
         pc = p2;
+        printout("invoke");
     }
 
     private int spp(int p1, int p2, int p3) {
-        if(p1 == 0)
+        if (p1 == 0)
             return p2;
         else
-            return spp(p1-1, stack.get(p3+3), stack.get(p3+4));
+            return spp(p1 - 1, stack.get(p3 + 3), stack.get(p3 + 4));
     }
 
     private int sfp(int p1, int p2, int p3) {
-        if(p1 == 0)
+        if (p1 == 0)
             return p3;
         else
-            return sfp(p1-1, stack.get(p3+3), stack.get(p3+4));
+            return sfp(p1 - 1, stack.get(p3 + 3), stack.get(p3 + 4));
     }
 
     private void returnn() {
         int res = stack.get(top);
         top = pp;
         pc = stack.get(fp);
-        pp = stack.get(fp+1);
-        pp = stack.get(fp+2);
+        pp = stack.get(fp + 1);
+        fp = stack.get(fp + 2);
         stack.set(top, res);
+        printout("returnn");
     }
 
     private void pop() {
         top--;
         pc++;
+        printout("pop");
     }
-
-    /*
-    public final static int CONST = 1;
-    public final static int LOAD = 2;
-    public final static int STORE = 3;
-    public final static int ADD = 4;
-    public final static int SUB = 5;
-    public final static int MUL = 6;
-    public final static int DIV = 7;
-    public final static int LT = 8;
-    public final static int GT = 9;
-    public final static int EQ = 10;
-    public final static int NEQ = 11;
-    public final static int IFZERO = 12;
-    public final static int GOTO = 13;
-    public final static int HALT = 14;
-    public final static int NOP = 15;
-    public final static int INVOKE = 16;
-    public final static int RETURN = 17;
-    public final static int POP = 18;
-    */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
--- src/main/java/de/unitrier/st/uap/w21/tram/Main.java
+++ src/main/java/de/unitrier/st/uap/w21/tram/Main.java
@@ -1,12 +1,179 @@
 
 package de.unitrier.st.uap.w21.tram;
-final class Main
-{
-	private Main(){}
-	
-	public static void main(String[] argv)
-	{
-		// TODO: Create an instance of the abstract machine with respective parameters
-		System.out.println(new AbstractMachine(Instruction.program1).interpret());
-	}
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
+final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] argv) {
+        // TODO: Create an instance of the abstract machine with respective parameters
+        //System.out.println(new AbstractMachine(Instruction.program1).interpret());
+
+        ArrayList<String> stack = new ArrayList<>();
+        String dateiname = argv[0];
+        Scanner scan = null;
+        try {
+            scan = new Scanner(new File(dateiname));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String linesplit = line.split(":")[1];
+            stack.add(linesplit);
+        }
+        scan.close();
+        ArrayList<Instruction> instructions = new ArrayList<>();
+        Iterator<String> iter = stack.iterator();
+        while (iter.hasNext()) {
+
+            String iterstr = iter.next();
+            String opcode = iterstr.split("[\\\\(||\\\\)]")[0];
+            String parameters;
+            String pr1 = null, pr2 = null, pr3 = null;
+
+            try {
+                if (iterstr.indexOf("(")!=-1) {
+                    parameters = iterstr.split("[\\(||\\)]")[1];
+                    String[] prs = parameters.split(",");
+
+                    pr1 = prs.length >= 1 ? prs[0] : null;
+                    pr2 = prs.length >= 2 ? prs[1] : null;
+                    pr3 = prs.length >= 3 ? prs[2] : null;
+               }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+
+            Instruction instruction = null;
+            switch (opcode) {
+                case "const":
+                    instruction = new Instruction(Instruction.CONST, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "load":
+                    instruction = new Instruction(Instruction.LOAD, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "store":
+                    instruction = new Instruction(Instruction.STORE, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "add":
+                    instruction = new Instruction(Instruction.ADD, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "sub":
+                    instruction = new Instruction(Instruction.SUB, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "mul":
+                    instruction = new Instruction(Instruction.MUL, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "div":
+                    instruction = new Instruction(Instruction.DIV, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "lt":
+                    instruction = new Instruction(Instruction.LT, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "gt":
+                    instruction = new Instruction(Instruction.GT, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "eq":
+                    instruction = new Instruction(Instruction.EQ, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "neq":
+                    instruction = new Instruction(Instruction.NEQ, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "ifzero":
+                    instruction = new Instruction(Instruction.IFZERO, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "goto":
+                    instruction = new Instruction(Instruction.GOTO, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "halt":
+                    instruction = new Instruction(Instruction.HALT, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "nop":
+                    instruction = new Instruction(Instruction.NOP, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "invoke":
+                    instruction = new Instruction(Instruction.INVOKE, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "return":
+                    instruction = new Instruction(Instruction.RETURN, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+                case "pop":
+                    instruction = new Instruction(Instruction.POP, pr1 != null ? Integer.parseInt(pr1) : null,
+                            pr2 != null ? Integer.parseInt(pr2) : null,
+                            pr3 != null ? Integer.parseInt(pr3) : null);
+                    instructions.add(instruction);
+                    break;
+            }
+        }
+        Instruction[] instructionsArray = new Instruction[instructions.size()];
+        int i = 0;
+        Iterator<Instruction> instructionIterator = instructions.iterator();
+        while (instructionIterator.hasNext()) {
+            instructionsArray[i] = instructionIterator.next();
+            i++;
+        }
+
+        AbstractMachine abstractMachine = new AbstractMachine(instructionsArray);
+        System.out.println(abstractMachine.interpret());
+    }
 }
